e9be3c37b6e378e1379f3d63bf3cf6f9af60eea3
underscore504 <127985532+underscore504@users.noreply.github.com>
2023-08-31 17:57:07 -0400

    Updated index.js (toggling)

--- scripts/index.js
+++ scripts/index.js
@@ -33,10 +33,36 @@ var creatureToPosition = {
 
 var currentCreature = "frog"
 
+var hatsOpen = true
+var creaturesOpen = true;
+
 function handle(e) {
     var img = "resources/images/" + e.target.id + ".png"
     var valid = validID[e.target.id]
 
+    if (e.target.id =="hat_toggle") {
+      if (hatsOpen == true) {   
+        document.getElementById("hat_selection").style.marginTop = "30vh"
+        document.getElementById("hat_toggle").style.bottom = "0"
+      } else {
+        document.getElementById("hat_selection").style.marginTop = "0"
+        document.getElementById("hat_toggle").style.bottom = "30vh"
+      }
+      hatsOpen = !hatsOpen
+    }
+
+    if (e.target.id =="creature_toggle") {
+        if (creaturesOpen == true) {   
+          document.getElementById("creature_selection").style.marginTop = "30vh"
+          document.getElementById("creature_toggle").style.bottom = "0"
+        } else {
+            document.getElementById("creature_selection").style.marginTop = "0"
+            document.getElementById("creature_toggle").style.bottom = "30vh"
+        }
+        creaturesOpen = !creaturesOpen
+      }
+    
+  
     if (valid) {
         if (valid==1) {
             document.getElementById("creature_image").src = img
